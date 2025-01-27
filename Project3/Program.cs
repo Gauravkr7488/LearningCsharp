abecfde85aef47c604bfde3697288a300fea7aa4
Gaurav Kumar <gauravkr748800@gmail.com>
2025-01-27 14:21:18 +0530

    Added project3

---
+++ Project3/Program.cs
@@ -0,0 +1,16 @@
+﻿Random dice = new();
+
+int roll1 = dice.Next(1, 6);
+int roll2 = dice.Next(1, 6);
+int roll3 = dice.Next(1, 6);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"The Total is:{total}");
+
+if (total < 10)
+{
+    Console.WriteLine("Damn thats low");
+}else{
+    Console.WriteLine("Good enough");
+}
