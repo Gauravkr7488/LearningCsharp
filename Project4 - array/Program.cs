ce555fa0104c531a416037803395cbc7b0d8c069
Gaurav Kumar <gauravkr748800@gmail.com>
2025-01-27 15:20:31 +0530

    added project 4

---
+++ Project4 - array/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] fruit = new string[3];
+
+fruit[0] = "apple";
+fruit[1] = "banana";
+fruit[2] = "grape";
+
+Console.WriteLine(fruit[0]);
+Console.WriteLine(fruit[1]);
+Console.WriteLine(fruit[2]);
