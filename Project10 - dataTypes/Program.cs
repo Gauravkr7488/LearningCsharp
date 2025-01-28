73d3eee3de6721feca100bd625742fa353b1cad1
Gaurav Kumar <gauravkr748800@gmail.com>
2025-01-28 16:28:53 +0530

    added project 10

---
+++ Project10 - dataTypes/Program.cs
@@ -0,0 +1,41 @@
+﻿// Console.WriteLine("Signed integral types:");
+
+// Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+// Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+// Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+// Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+// int first = 2;
+// string second = "4";
+// int result = first + int.Parse(second);
+// Console.WriteLine(result);
+
+// int myInt = 3;
+// Console.WriteLine($"int: {myInt}");
+
+// decimal myDecimal = myInt;
+// Console.WriteLine($"decimal: {myDecimal}");
+
+// decimal myDecimal = 3.14m;
+// Console.WriteLine($"decimal: {myDecimal}");
+
+// int myInt = (int)myDecimal;
+// Console.WriteLine($"int: {myInt}");
+
+
+// string first = "5.35";
+// string second = "7";
+// decimal sum = decimal.Parse(first) + decimal.Parse(second);
+// Console.WriteLine(sum);
+
+string input = "123";
+int number;
+
+if (int.TryParse(input, out number))
+{
+    Console.WriteLine($"Conversion succeeded! The number is {number}");
+}
+else
+{
+    Console.WriteLine("Conversion failed. The input is not a valid number.");
+}
