e2b3df1f5ea028ced100e50cc9e9311dd25e2ac6
Gaurav Kumar <gauravkr748800@gmail.com>
2025-01-28 13:26:52 +0530

    added project 9

---
+++ Project9 - do-while/Program.cs
@@ -0,0 +1,12 @@
+﻿int A = 5;
+do
+{
+    Console.WriteLine($"This is the do - while loop speaking \n The value of A is {A}, So A is less than 5");
+    A++;
+} while (A < 5);
+
+while (A < 5)
+{
+    Console.WriteLine($"This is the while loop speaking \nThe value of A is {A}, So A is less than 5");
+    A++;
+}
