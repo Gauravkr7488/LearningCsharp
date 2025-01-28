c9e5049a944067abef1792c4934970d43a278ae1
Gaurav Kumar <gauravkr748800@gmail.com>
2025-01-28 12:42:02 +0530

    added project7

---
+++ Project7 - Ternary Conditional Operator/Program.cs
@@ -0,0 +1,10 @@
+﻿// int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
+// Console.WriteLine($"Discount: {discount}");
+
+// int i = 10;
+// i < discount ? discount = 10 : discount = 50;
+
+Random CoinFlip = new();
+int CoinFace = CoinFlip.Next(1, 2);
+Console.WriteLine($"{(CoinFace == 1 ? "heads" : "tails")}");
