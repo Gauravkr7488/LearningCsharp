0d27c7b88bffd12010ddfca660b3640c2ef50d04
Gaurav Kumar <gauravkr748800@gmail.com>
2025-01-29 19:19:22 +0530

    added project 11

---
+++ Project11 - Operations on Arrays/Program.cs
@@ -0,0 +1,51 @@
+﻿// string[] pallets = [ "B14", "A11", "B12", "A13" ];
+
+// Console.WriteLine("Sorted...");
+// Array.Sort(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string[] pallets = [ "B14", "A11", "B12", "A13" ];
+
+// Console.WriteLine("Sorted...");
+// // Array.Sort(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet.ToLower()}");
+// }
+
+// Console.WriteLine("");
+// Console.WriteLine("Reversed...");
+// Array.Reverse(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// int[] fruits = [11, 3, 22, 0, 2];
+
+// Array.Resize( ref fruits, 2);
+
+// foreach (var item in fruits)
+// {
+//     Console.WriteLine(item);
+// }
+
+// string value = "abc123";
+// char[] valueArray = value.ToCharArray();
+// Array.Reverse(valueArray);
+// string result = new string(valueArray);
+// Console.WriteLine(result);
+
+// string name = "gaurav";
+// char[] chars = name.ToCharArray();
+// foreach (var item in chars)
+// {
+//     Console.WriteLine(item);
+// }
+
+// string newName = new(chars);
+
+// Console.WriteLine(newName);
