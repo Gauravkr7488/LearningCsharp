88c8cf0e009a822353a77fbf2b3f9f807f1284a3
Gaurav Kumar <gauravkr748800@gmail.com>
2025-01-29 21:24:41 +0530

    added project - 12

---
+++ Project12 - Modifying the contents of a string/Program.cs
@@ -0,0 +1,7 @@
+﻿string name = "gaurav";
+char[] chars = name.ToCharArray();
+int position = name.IndexOf('a'); // Learning about IndexOf()
+Console.WriteLine(position);
+Console.WriteLine(name.Substring(1, 2)); // Learning about Substring()
+Console.WriteLine(name.Remove(1, 2)); // Learning about Remove()
+Console.WriteLine(name.Replace('a', 'r')); // Learning about Substring()
