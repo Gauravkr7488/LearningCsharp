0d249552cc89565ae3dbbae232eca36266f414b5
Gaurav Kumar <gauravkr748800@gmail.com>
2025-01-30 19:51:25 +0530

    added project 14

---
+++ Project14 - Debugging/Program.cs
@@ -0,0 +1,70 @@
+﻿// /* 
+// This code uses a names array and corresponding methods to display
+// greeting messages
+// */
+
+// string[] names = new string[] { "Sophia", "Andrew", "AllGreetings" };
+
+// string messageText = "";
+
+// foreach (string name in names)
+// {
+//     if (name == "Sophia")
+//         messageText = SophiaMessage();
+//     else if (name == "Andrew")
+//         messageText = AndrewMessage();
+//     else if (name == "AllGreetings")
+//         messageText = SophiaMessage();
+//         messageText = messageText + "\n\r" + AndrewMessage();
+
+//     Console.WriteLine(messageText + "\n\r");
+// }
+
+// bool pauseCode = true;
+// while (pauseCode == true);
+
+// static string SophiaMessage()
+// {
+//     return "Hello, my name is Sophia.";
+// }
+
+// static string AndrewMessage()
+// {
+//     return "Hi, my name is Andrew. Good to meet you.";
+// }
+
+// double float1 = 3000.0;
+// double float2 = 0.0;
+// int number1 = 3000;
+// int number2 = 0;
+
+// Console.WriteLine(float1 / float2);
+// Console.WriteLine(number1 / number2);
+// Console.WriteLine("Exit program");
+
+try
+{
+    Process1();
+}
+catch
+{
+    Console.WriteLine("An exception has occurred");
+}
+
+Console.WriteLine("Exit program");
+
+static void Process1()
+{
+    WriteMessage();
+}
+
+static void WriteMessage()
+{
+    double float1 = 3000.0;
+    double float2 = 0.0;
+    int number1 = 3000;
+    int number2 = 0;
+
+    Console.WriteLine(float1 / float2);
+    Console.WriteLine(number1 / number2);
+}
