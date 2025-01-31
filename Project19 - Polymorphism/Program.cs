e04c62054fa6e6a34e08c64955d6f72d6a96f3f5
Gaurav Kumar <gauravkr748800@gmail.com>
2025-01-31 16:29:21 +0530

    added project - 19

---
+++ Project19 - Polymorphism/Program.cs
@@ -0,0 +1,34 @@
+﻿class Animal
+{
+    public string name;
+    public virtual void Attack()
+    {
+        Console.WriteLine($"{name} just bit you");
+    }
+}
+
+class Monkey : Animal
+{
+    public void Greet()
+    {
+        Console.WriteLine($"This is {name}, he is a bad monkey");
+    }
+
+    public override void Attack(){
+        Console.WriteLine($"{name} just kicked you in the face.");
+    }
+}
+
+class Application
+{
+
+    static void Main()
+    {
+        Monkey Sam = new()
+        {
+            name = "Sam"
+        };
+        Sam.Greet();
+        Sam.Attack();
+    }
+}
