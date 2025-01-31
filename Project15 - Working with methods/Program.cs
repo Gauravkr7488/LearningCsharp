d4be34fe51a6b20aa45146160a9cf66d9ad36a9e
Gaurav Kumar <gauravkr748800@gmail.com>
2025-01-31 13:21:46 +0530

    added project 15

---
+++ Project15 - Working with methods/Program.cs
@@ -0,0 +1,33 @@
+﻿// Named arguments
+class Progmamm
+{
+
+    static void makeSound(string animalOne, string animalTwo)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{animalOne}");
+            Console.WriteLine($"{animalTwo}");
+        }
+
+    }
+
+
+
+    // Method Overloading
+
+    static void makeSound(int one, int two)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(one);
+            Console.WriteLine(two);
+        }
+    }
+    static void Main()
+    {
+
+        makeSound(1, 2);
+        makeSound(animalTwo: "cat", animalOne: "dog");
+    }
+}
