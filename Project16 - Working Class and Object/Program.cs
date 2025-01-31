7efe8b37ef0c46a1df6a07b3f9238048e1d037f1
Gaurav Kumar <gauravkr748800@gmail.com>
2025-01-31 15:36:22 +0530

    added project 16

---
+++ Project16 - Working Class and Object/Program.cs
@@ -0,0 +1,46 @@
+﻿// class Person{
+//     string name;
+//     string gender;
+//     int age;
+//     string job;
+
+//     public void Greet(){
+//         Console.Write($"Hi {name}");
+//     }
+
+//     static void Main()
+//     {
+//         Person Bob = new();
+//         Bob.name = "Bob";
+//         Bob.Greet();
+//     }
+// }
+
+// Constructors
+
+class Person
+{
+    string name;
+        string gender;
+        int age;
+        string job;
+
+    public Person()
+    {
+        gender = "male";
+        age = 150;
+        job = "Harbinger of death";
+    }
+
+    public void Greet()
+    {
+        Console.Write($"Hi {name} how is you'r {job} going?");
+    }
+
+    static void Main()
+    {
+        Person Bob = new();
+        Bob.name = "Bob";
+        Bob.Greet();
+    }
+}
