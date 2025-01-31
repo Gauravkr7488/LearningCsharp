578a0dbe849d44fe19dbf726a7d3a627646f7ce7
Gaurav Kumar <gauravkr748800@gmail.com>
2025-01-31 16:15:17 +0530

    added project 18

---
+++ Project18 - Inheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿class Animal
+{
+    public string name;
+    public void Attack()
+    {
+        Console.WriteLine($"{name} just bit you");
+    }
+}
+
+class Monkey : Animal
+{
+    public void Greet()
+    {
+        Console.WriteLine($"This is {name}, he is a bad monkey");
+    }
+}
+
+class Application
+{
+
+    static void Main()
+    {
+        Monkey Sam = new()
+        {
+            name = "Sam"
+        };
+        Sam.Greet();
+        Sam.Attack();
+    }
+}
