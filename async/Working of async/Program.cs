5ab36bfded558989b27f331fec562a8be1306825
Gaurav Kumar <gauravkr748800@gmail.com>
2025-02-17 11:22:41 +0530

    Completed the Project

--- async/Working of async/Program.cs
+++ async/Working of async/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿class GetData
+{
+    static async Task DownloadFile()
+    {
+        Console.WriteLine("The File is starting to download");
+        await Task.Delay(5000);
+        Console.WriteLine("File is downloaded");
+    }
+    static async Task Main()
+    {
+        Task DownloadTask = DownloadFile();
+        Console.WriteLine("The file is being downloaded in the background");
+        await DownloadTask;
+    }
+}
